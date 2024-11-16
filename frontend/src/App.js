15f7c418c6ec50eaaf6c502bb79f8c680a03b1d4
Weltraumbiene <ben@benben.de>
2024-11-16 22:13:05 +0100

    Frontend Server startbar eingerichtet

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,51 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import './App.css';
+
+import { Routes, Route } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
+
+import Home from './components/Home';
+import Contact from './components/Contact';
+import Navigation from './components/Navigation';
+import Login from './components/Login';
+import UserProfile from './components/UserProfile';
 
 function App() {
-    const [message, setMessage] = useState("");
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        fetch(`${process.env.REACT_APP_API_URL}/profile`, {
-            headers: {
-                'Authorization': `Bearer YOUR_TOKEN_HERE` // Token hier einsetzen
-            }
-        })
-        .then(response => response.json())
-        .then(data => setMessage(data.message))
-        .catch(error => console.error('Error:', error));
-    }, []);
+  useEffect(() => {
+    // Prüfen, ob ein Token und userId im Local Storage vorhanden ist
+    const token = localStorage.getItem('token');
+    const storedUserId = localStorage.getItem('userId');
 
-    return (
-        <div>
-            <h1>Welcome to the Frontend</h1>
-            <p>{message}</p>
-        </div>
-    );
+    if (token && storedUserId) {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
+  const handleLogout = () => {
+    localStorage.removeItem('token');
+    localStorage.removeItem('userId');
+    setIsLoggedIn(false);
+    navigate('/');
+  };
+
+  return (
+    <div className="App">
+      <Navigation isLoggedIn={isLoggedIn} handleLogout={handleLogout} />
+
+      <Container className="mt-4">
+        <Routes>
+          <Route path="/" element={<Home isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/login" element={<Login isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />} />
+          <Route path="/profile" element={<UserProfile />} />
+        </Routes>
+      </Container>
+    </div>
+  );
 }
 
 export default App;
