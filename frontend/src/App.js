7c677170c3ec3462150ce89e6ae5fd3e19133c0d
Weltraumbiene <ben@benben.de>
2024-11-19 21:50:03 +0100

    Die Login-Funktion steht!!!

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './components/Home';
 import Login from './components/Login';
+import Shop from './components/Shop';
 import Contact from './components/Contact';
 import Imprint from './components/Imprint';
 import ScrollButtons from './components/ScrollButtons';
 
 const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false); // Zustand für den Login-Status
+
   return (
     <div className="App">
       <Header />
       <Home />
-      <Login />
+      <Login setIsLoggedIn={setIsLoggedIn} /> {/* Funktion als Prop übergeben */}
+      <Shop isLoggedIn={isLoggedIn} /> {/* Login-Status an Shop weitergeben */}
       <Contact />
       <Imprint />
       <ScrollButtons />
